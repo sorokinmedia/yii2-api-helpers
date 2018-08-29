677c95b54afa7df846b2422d23cfb04d4afcd128
ma3obblu <Ma3oBblu@gmail.com>
2018-08-29 15:55:11 +0400

    [fix] namespace

--- src/CrudModels/filters/CrudInputNumberFilter.php
+++ src/CrudModels/filters/CrudInputNumberFilter.php
@@ -1,11 +1,11 @@
 <?php
-namespace sorokinmedia\api_helpers\CrudModel\filters;
+namespace sorokinmedia\api_helpers\CrudModels\filters;
 
 use sorokinmedia\api_helpers\CrudModels\CrudFilter;
 
 /**
  * Class CrudInputNumberFilter
- * @package sorokinmedia\api_helpers\CrudModel\filters
+ * @package sorokinmedia\api_helpers\CrudModels\filters
  */
 class CrudInputNumberFilter extends CrudFilter
 {
