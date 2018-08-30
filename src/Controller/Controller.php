3bf6f4d9ff55e81ce43f587b0a5263a3e4eed6bf
ma3obblu <Ma3oBblu@gmail.com>
2018-08-30 14:57:12 +0400

    [fix] apidoc trait remove

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -17,25 +17,6 @@ use yii\web\Response;
  */
 class Controller extends \yii\rest\Controller
 {
-    /**
-     * @apiDefine SuccessRespond
-     *
-     * @apiSuccess {Number} status Статус ответа (0 - успешное выполнение, 10 - лог, 100 - ошибка)
-     * @apiSuccess {Object[]} messages Сообщение
-     * @apiSuccess {String} messages.message Текст сообщения
-     * @apiSuccess {Number} messages.type Тип сообщения (0 - успешное выполнение, 1 - ошибка валидации, 2 - серверная ошибка, 3 - лог)
-     */
-
-    /**
-     * @apiDefine ErrorRespond
-     *
-     * @apiError {Null} response Ответ сервиса
-     * @apiError {Number} status Статус ответа (0 - успешное выполнение, 10 - лог, 100 - ошибка)
-     * @apiError {Object[]} messages Сообщение
-     * @apiError {String} messages.message Текст сообщения
-     * @apiError {Number} messages.type Тип сообщения (0 - успешное выполнение, 1 - ошибка валидации, 2 - серверная ошибка, 3 - лог)
-     */
-
     public $serializer = Serializer::class; // замена сериалайзера
 
     /**
