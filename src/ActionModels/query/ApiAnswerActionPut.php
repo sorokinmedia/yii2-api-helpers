f9baefb0fae7b8229985aaf75372f3406be7ee18
ivanmenalkin <ivan.menyalkin@gmail.com>
2019-04-14 22:05:48 +0400

    [improvements] added put action.

---
+++ src/ActionModels/query/ApiAnswerActionPut.php
@@ -0,0 +1,25 @@
+<?php
+namespace sorokinmedia\api_helpers\ActionModels\query;
+
+use sorokinmedia\api_helpers\ActionModels\ApiAnswerAction;
+
+/**
+ * Класс, описывающий действие в виде PUT запроса
+ * Необходимо задание обязательных параметров: id, name, url
+ *
+ * Class ApiAnswerActionPut
+ * @package sorokinmedia\api_helpers\ActionModels\query
+ */
+class ApiAnswerActionPut extends ApiAnswerAction
+{
+    /**
+     * ApiAnswerActionGet constructor.
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+        $this->type = self::TYPE_QUERY;
+        $this->method = self::METHOD_PUT;
+    }
+}
