ae010e59531a72b0df43f4ac427b5615d36c6c13
ma3obblu <Ma3oBblu@gmail.com>
2019-07-28 11:28:55 +0400

    [feature] add apiAnswerLog classes to work with cache

---
+++ src/ApiAnswer/ApiAnswerLogFromCache.php
@@ -0,0 +1,38 @@
+<?php
+namespace sorokinmedia\api_helpers\ApiAnswer;
+
+use sorokinmedia\api_helpers\Controller\ApiController;
+
+/**
+ * Класс, описывающий ответ сервера, при котором важна передача ответа с данными
+ * Сообщение не выводится пользователю
+ * Модификация: дефолтное сообщение, что данные взяты из кеша
+ *
+ * Class ApiAnswerLogFromCache
+ * @package sorokinmedia\api_helpers\ApiAnswer
+ */
+class ApiAnswerLogFromCache extends ApiAnswer
+{
+    /**
+     * ApiAnswerLog constructor.
+     * @param string|null $message
+     * @param null $response
+     */
+    public function __construct(string $message = null, $response = null)
+    {
+        if ($message === null){
+            $message = \Yii::t('app', 'Данные получены из кеша');
+        }
+        parent::__construct([
+            'response' => $response,
+            'messages' => [
+                new RestMessage([
+                    'type' => RestMessage::TYPE_LOG,
+                    'message' => $message,
+                    'targetField' => null,
+                ]),
+            ],
+            'status' => ApiController::STATUS_SUCCESS,
+        ]);
+    }
+}
---
+++ src/ApiAnswer/ApiAnswerLogInsertCache.php
@@ -0,0 +1,38 @@
+<?php
+namespace sorokinmedia\api_helpers\ApiAnswer;
+
+use sorokinmedia\api_helpers\Controller\ApiController;
+
+/**
+ * Класс, описывающий ответ сервера, при котором важна передача ответа с данными
+ * Сообщение не выводится пользователю
+ * Модификация: дефолтное сообщение, что данные записаны в кеш
+ *
+ * Class ApiAnswerLogInsertCache
+ * @package sorokinmedia\api_helpers\ApiAnswer
+ */
+class ApiAnswerLogInsertCache extends ApiAnswer
+{
+    /**
+     * ApiAnswerLog constructor.
+     * @param string|null $message
+     * @param null $response
+     */
+    public function __construct(string $message = null, $response = null)
+    {
+        if ($message === null){
+            $message = \Yii::t('app', 'Данные получены и записаны в кеш');
+        }
+        parent::__construct([
+            'response' => $response,
+            'messages' => [
+                new RestMessage([
+                    'type' => RestMessage::TYPE_LOG,
+                    'message' => $message,
+                    'targetField' => null,
+                ]),
+            ],
+            'status' => ApiController::STATUS_SUCCESS,
+        ]);
+    }
+}
